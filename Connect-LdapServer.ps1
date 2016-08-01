fba76546e0fadd2dffbe5bb75f15f4289e54650b
Glen Buktenica <glen@buktenica.com>
2016-08-01 13:36:55 +0800

    Update Connect-LdapServer.ps1

--- Connect-LdapServer.ps1
+++ Connect-LdapServer.ps1
@@ -42,7 +42,7 @@
 
     .NOTES  
         Author     : Glen Buktenica
-	    Version    : 1.0.0.0 20160704 Initial Build  
+        Version    : 1.0.0.1 20160801 Alpha Release 1 
     #> 
     [CmdletBinding()]
     Param
